cebb3cb71d7e3ccf844a22859404da67dad45b4a
Párek8 <petrpolak11@zak.spsul.cz>
2024-02-20 21:53:16 +0100

    Fix: Infinite Crafting Bug Fixed

--- Assets/Scripts/CraftingSystem.cs
+++ Assets/Scripts/CraftingSystem.cs
@@ -13,16 +13,13 @@ public class CraftingSystem : MonoBehaviour
         foreach (Recipes recipe in recipes)
         {
             if (HasItemsOnGround(recipe.requiredItems))
-            {
                 CreateItem(recipe);
-                RemoveItems(recipe.requiredItems);
-            }
         }
     }
     private void CreateItem(Recipes recipe)
     {
-        Vector2 spawnPosition1 = new Vector2(transform.position.x, transform.position.y);
-        Instantiate(recipe.craftedItemPrefab, spawnPosition1, Quaternion.identity);
+        Instantiate(recipe.craftedItemPrefab, transform.position, Quaternion.identity);
+        RemoveItems(recipe.requiredItems);
     }
     private void RemoveItems(List<string> itemNames)
     {
@@ -43,51 +40,40 @@ public class CraftingSystem : MonoBehaviour
         {
             int requiredItemCount = groundItems.FindAll(item => item.Item.itemName == requiredItemName).Count;
             if (requiredItemCount < inRecipeItems[requiredItemName])
-            {
                 return false;
-            }
         }
         return true;
     }
     private void CheckRadiusForItems()
     {
+        groundItems.Clear();
+
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.5f);
 
         foreach (Collider2D collider in colliders)
         {
             ItemPickup itemPickup;
-            if (collider.TryGetComponent<ItemPickup>(out itemPickup))
-            {
+            if (collider.TryGetComponent(out itemPickup))
                 if (!groundItems.Contains(itemPickup))
-                {
                     groundItems.Add(itemPickup);
-                }
-            }
         }
     }
     Dictionary<string, int> GetRequiredItemsCounts(List<string> itemNames) 
     {
         Dictionary<string, int> items = new();
-        foreach (string itemName in itemNames) 
-        {
+
+        foreach (string itemName in itemNames)
             if (items.ContainsKey(itemName))
-            {
                 items[itemName]++;
-            }
             else
-            {
                 items[itemName] = 1;
-            }
-        }
+
         return items;
-    }    
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            TryCraft();
-        }
         CheckRadiusForItems();
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            TryCraft();
     }
-
 }
